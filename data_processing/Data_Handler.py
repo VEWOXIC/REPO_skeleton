8260cd2f415de6eb6aceb63da50653e76debfb97
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2022-06-26 04:10:42 +0800

    Fix Bugs, Update Codes

--- data_processing/Data_Handler.py
+++ data_processing/Data_Handler.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 from utils import data_utils
+from utils.timefeatures import time_features
 import pandas as pd
 
 
@@ -17,41 +18,51 @@ class Dataset_Custom(Dataset):
         self.flag = flag
         self.lookback = cfg['data']['lookback']
         self.horizon = cfg['data']['horizon']
+        self.timeStampFreq = cfg['data']['freq'] # Choose a time feature
+
         self.__read_data__()
         # need to be implemented
         # transformer based methods have label_len
         
-        
         # 以下是add time in a day的function   還有以下代碼的命名以及if else有點針對,等等我會稍微改一下代碼讓這些處理更general 
-    def add_time_dim_in_MTGNN(self):
-        time_ind = (self.data['date'].astype("datetime64[h]") - self.data['date'].astype("datetime64[D]")) / np.timedelta64(1, "D")
-        self.time_in_day = np.tile(time_ind, [1, self.cfg['model']['num_nodes'], 1]).transpose((2, 1, 0))
+
+    def add_timeFeature(self, data):
+        data['date'] = pd.to_datetime(data.date)
+        data_stamp = time_features(pd.to_datetime(data['date'].values), freq=self.timeStampFreq)
+        data_stamp = data_stamp.transpose(1, 0)
+        return data_stamp # cfg['data']['freq']==“h" -> data_stamp = [HourOfDay, DayOfWeek, DayOfMonth, DayOfYear] MTGNN就拿第一个
         
-      
     def __read_data__(self):
         self.scaler = data_utils.get_scaler(self.cfg['data']['scalar'])
         path = self.cfg["data"]['path']
         self.data = pd.read_csv(path)
-        if self.cfg['model']['model_name'] == 'MTGNN':
-            self.add_time_dim_in_MTGNN()
-        self.data = self.data.drop(self.data.columns[[i for i in range(self.data.shape[1]-self.cfg['data']['channel'])]] ,axis = 1)
+
         num_train = int(len(self.data) * self.cfg["data"]["train_ratio"])
         num_test = int(len(self.data) * self.cfg["data"]["test_ratio"])
         num_vali = len(self.data) - num_train - num_test
         boarder = {'train':[0,num_train],'valid':[num_train,num_train+num_vali],'test':[num_train+num_vali,len(self.data)-1]}
 
+
+        self.data_stamp = self.add_timeFeature(self.data[['date']][boarder[self.flag][0]:boarder[self.flag][1]])
+
+        self.data = self.data.drop(self.data.columns[[i for i in range(self.data.shape[1]-self.cfg['data']['channel'])]] ,axis = 1)
+        
         train_data = self.data[boarder["train"][0]: boarder["train"][1]].values
         self.scaler.fit(train_data)
         self.data = self.data[boarder[self.flag][0]: boarder[self.flag][1]]
         self.data = self.scaler.transform(self.data.values)
+
         # 单变量/多变量
        
     def __getitem__(self, index):
         # some model use time stamp
-        # to be implemented
-        if self.cfg['model']['model_name'] == "MTGNN":
-            return self.data[index:index+self.lookback], self.data[index+self.lookback:index+self.lookback+self.horizon], self.time_in_day[index:index+self.lookback], self.time_in_day[index+self.lookback:index+self.lookback+self.horizon]
-        else:
-            return self.data[index:index+self.lookback], self.data[index+self.lookback:index+self.lookback+self.horizon]
+        x = self.data[index:index+self.lookback]
+        y = self.data[index+self.lookback:index+self.lookback+self.horizon]
+
+        timestamp_x = self.data_stamp[index:index+self.lookback]
+        timestamp_y = self.data_stamp[index+self.lookback:index+self.lookback+self.horizon]
+
+        return x, y, timestamp_x, timestamp_y
+    
     def __len__(self):
         return self.data.shape[0]-self.horizon-self.lookback+1
