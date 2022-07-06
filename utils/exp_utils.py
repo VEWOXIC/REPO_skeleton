d4f62804c548198b9d380093b1b06bed598e5d76
Carlos <90025401+943994056@users.noreply.github.com>
2022-07-07 02:07:47 +0800

    Update exp_utils.py

--- utils/exp_utils.py
+++ utils/exp_utils.py
@@ -115,7 +115,7 @@ class EarlyStopping:
 
 def masked_mae_loss(y_pred, y_true):
     mask = (y_true != 0).float()
-    mask /= (mask.mean()+ (1e-4) )
+    mask /= (mask.mean())
     loss = torch.abs(y_pred - y_true)
     loss = loss * mask
     loss[loss != loss] = 0
@@ -129,7 +129,7 @@ def masked_mae_torch(preds, labels, null_val=np.nan):
     else:
         mask = labels.ne(null_val)
     mask = mask.float()
-    mask /= torch.mean(mask)
+    mask /= (torch.mean(mask) + 1e-7)
     mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
     loss = torch.abs(torch.sub(preds, labels))
     loss = loss * mask
