f5c238694ecb89808172ef56a2d17e505170401e
zhyu9 <62586479+zhyu9@users.noreply.github.com>
2022-07-09 20:00:59 +0800

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,5 @@
 from .LinearLayer.LinearLayer import LinearLayer
+from .SCINet.SCINet import SCINet
+from .MTGNN.MTGNN import MTGNN
+from .Informer.model import Informer
+from .Autoformer.Autoformer import Autoformer
