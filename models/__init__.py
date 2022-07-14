55e8cbf533dab81f3fe62da0216c4991c6956e72
byydzh <89905429+byydzh@users.noreply.github.com>
2022-07-15 07:53:59 +0800

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from .LinearLayer.LinearLayer import LinearLayer
 from .SCINet.SCINet import SCINet
 from .MTGNN.MTGNN import MTGNN
+from .RNN.RNN import RNN
+from .InceptionTime.InceptionTime import InceptionTime
+from .TransformerModel.TransformerModel import TransformerModel
