665c3ba1d34d1f6fed91f602f9317117362dd8ce
khchoi0 <khchoi0@users.noreply.github.com>
2022-11-30 04:11:51 +0000

    Auto code format

--- docs/conf.py
+++ docs/conf.py
@@ -13,23 +13,22 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 
-project = 'REPO_skeleton'
-copyright = '2022, .'
-author = '.'
-release = '.'
+project = "REPO_skeleton"
+copyright = "2022, ."
+author = "."
+release = "."
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
