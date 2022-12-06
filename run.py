0b45ea9a0b20020272056c1b8aacdda838d743c1
zhyu9 <62586479+zhyu9@users.noreply.github.com>
2022-12-06 21:29:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+from experiments.exp_basic import *
 from datetime import datetime
 import argparse
 
@@ -25,27 +26,21 @@ if __name__ == '__main__':
         cfg =json.load(f)
     # else
 
-    model_save_dir = "cache/{}/{}/{}".format(
-        cfg["model"]["model_name"],
-        cfg["data"]["dataset_name"],
-        cfg["data"]["horizon"])
+
+    model_save_dir = 'cache/{}/{}/{}'.format(cfg['model']['model_name'], cfg['data']['dataset_name'], cfg['data']['horizon'])
     if not os.path.exists(model_save_dir):
         os.makedirs(model_save_dir)
-
-    model_name = cfg["model"]["model_name"]
-    dataset_name = cfg["data"]["dataset_name"]
+        
+    model_name = cfg['model']['model_name']
+    dataset_name = cfg['data']['dataset_name']
     exp = Exp_Basic(cfg, model_save_dir)
-    if cfg["exp"]["train"]["training"] or not os.path.exists(model_save_dir):
-        print(
-            "Start training for model:",
-            model_name,
-            " dataset:",
-            dataset_name)
+    if cfg['exp']['train']['training'] or not os.path.exists(model_save_dir):
+        print("Start training for model:", model_name, " dataset:", dataset_name)
         before_train = datetime.now().timestamp()
         print("===================Train-Start=========================")
         exp.train()
         after_train = datetime.now().timestamp()
-        print(f"Training took {(after_train - before_train) / 60} minutes")
+        print(f'Training took {(after_train - before_train) / 60} minutes')
         print("===================Train-End=========================")
     else:
         exp.load_model()
@@ -53,8 +48,4 @@ if __name__ == '__main__':
     before_evaluation = datetime.now().timestamp()
     exp.test()
     after_evaluation = datetime.now().timestamp()
-    print(
-        "Test/evaluation took: {} minutes".format(
-            (after_evaluation - before_evaluation) / 60
-        )
-    )
+    print('Test/evaluation took: {} minutes'.format((after_evaluation - before_evaluation) / 60))
