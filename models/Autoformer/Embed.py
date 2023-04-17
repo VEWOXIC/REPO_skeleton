76ae22fbb762b1e9019019524e67f16b26034f2a
zhyu9 <1155141497@link.cuhk.edu.hk>
2023-04-17 19:57:37 +0800

    Transformer model update

--- models/Autoformer/Embed.py
+++ models/Autoformer/Embed.py
@@ -130,5 +130,8 @@ class DataEmbedding_wo_pos(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x, x_mark):
-        x = self.value_embedding(x) + self.temporal_embedding(x_mark)
+        if x_mark is None:
+            x = self.value_embedding(x)
+        else:
+            x = self.value_embedding(x) + self.temporal_embedding(x_mark)
         return self.dropout(x)
