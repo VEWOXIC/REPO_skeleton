5270b41ac6eece33475e43884855a3721b9365a1
khchoi0 <khchoi0@users.noreply.github.com>
2023-04-23 22:41:12 +0000

    Auto code format

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -11,23 +11,22 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-project = 'TSF-Skeleton-Repository'
-copyright = '2023, .'
-author = '.'
-release = '1.0'
+project = "TSF-Skeleton-Repository"
+copyright = "2023, ."
+author = "."
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
